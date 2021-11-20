ef250702e3c0d795f7c84ecf641de92c2a2fa372
stovet <steven.topalian@gmail.com>
2021-11-20 10:49:27 -0500

    fight function, still not working

--- src/script.js
+++ src/script.js
@@ -4,16 +4,20 @@
 
 const randomDamage = () => {
     let number = Math.floor(Math.random() * 10) + 1
-    console.log(number)
+    console.log(number);
+    return number;
 }
 
 const chooseOption = (opt1, opt2) => {
     let randNum = Math.round( Math.random() )
+    console.log(randNum)
     return randNum === 0 ? opt1 : opt2;
 }
 
 function attackPlayer(health){
-    return health-= randomDamage();
+    health = health - randomDamage();
+    console.log(health)
+    return health;
 }
 
 const logHealth = (player, health) => {
@@ -25,5 +29,28 @@ const logDeath = (winner, loser) => {
 }
 
 const isDead = (health) => {
-    return health <= 0 ? true : false;
+    if(health <= 0){
+        return true;
+    } else return false;
+}
+
+function fight(player1, player2, player1Health, player2Health){
+    while(true){
+        let attacker = chooseOption(player1, player2)
+        if(attacker === player1){
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if(isDead(player2Health) === true){
+                logDeath(player1, player2);
+                break;
+            }
+        } else {
+            player1Health = attackPlayer(player1Health)
+            logHealth(player1, player1Health);
+            if(isDead(player1Health === true)){
+                logDeath(player2, player1);
+                break;
+            }
+        }
+    }
 }
